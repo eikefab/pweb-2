4efb5a605c844796861753fd01050e1a249e119c
Luiz Carlos Vilela <41112779+filipezaidan@users.noreply.github.com>
2022-01-10 00:15:54 -0300

    add functions for request

---
+++ app/post.js
@@ -0,0 +1,44 @@
+async function postFormDataAsJson({ url, formData }) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: formDataJsonString,
+    };
+
+    const response = await fetch(url, fetchOptions);
+
+    if (!response.ok) {
+        alert('Ops! Ocorreu algum erro...')
+        window.location.href = "index.html";
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+    window.location.href = "sucess.html";
+    return response.json();
+}
+
+async function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const url = form.action;
+
+    try {
+        const formData = new FormData(form);
+        const responseData = await postFormDataAsJson({ url, formData });
+        
+
+        console.log({ responseData });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const exampleForm = document.getElementById("example-form");
+exampleForm.addEventListener("submit", handleFormSubmit);
